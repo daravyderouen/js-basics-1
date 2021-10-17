6e4e07b6ce7e5a822c40ac07a86bea56aeb71b6f
Daravy DeRouen <daravyderouen@gmail.com>
2021-10-17 00:37:02 -0500

    Revised constraints for password.js

--- password.js
+++ password.js
@@ -1,5 +1,15 @@
-/*
-alert("Hello World!");
+
+
+const readline = require("readline");
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+reader.question("Hello! What is your name?", function(answer) {
+  console.log("Hi " + answer);
+});
 
 
 const str = `W l om  to p sswor  v li  tor tool 
@@ -11,29 +21,36 @@ X .  X     .     X  .  X.
 . .  .     .     X  .  X.         
                                   
 Welcome to password validator tool!`;
- console.log(str);
+console.log(str);
 
 
-prompt("Please validate your password.");
 
 console.log('Password must be at least 10 Characters');
 console.log('Password must contain at least one Uppercase letter & a number');
 
-if (passwordLength => 10){
-    console.log("Password was successful.");
-} else {
-    console.log("Password failed. Please try again.");
+let password ='iLove2Code';
+
+
+if (password.length > 10) {
+  console.log("Your password needs a minimum of four characters.")
+} else if (password.search(/[a-z]/) < 0) {
+  console.log("Your password needs a lower case letter.")
+} else if(password.search(/[A-Z]/) < 0) {
+  console.log("Your password needs an uppser case letter.")
+} else  if (password.search(/[0-9]/) < 0) {
+  console.log("Your password needs a number.")
+ } else {
+  console.log("You're in! Let's go!")
 }
-*/
 
-let password = "123ABCRbresoesD!";
+/*
 
-let passChecker = function(pass){
-  if ((pass.includes("123!@ABS")) && (pass.length > 10)) {
+  if  (pass.length > 10) {
   console.log("Congrats! You're logged in!");  
   } else {
     console.log("Failed! Try again!");
   }
 }
 
-console.log(passChecker(password));
+
+*/
