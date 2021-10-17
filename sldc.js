913a3d935c1c8da4121ebdb28671cada81938e1c
Daravy DeRouen <daravyderouen@gmail.com>
2021-10-17 13:48:31 -0500

    Revised sldc.js, took out parentheses

--- sldc.js
+++ sldc.js
@@ -18,14 +18,14 @@ Maintain
 /*
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
 */
- let planning = ("Planning is when one, or a team, establishes a framework in which the steps of software development are applied, an overall roadmap for the project. Methods can include Agile development, waterfall, etc.");
+ let planning = "Planning is when one, or a team, establishes a framework in which the steps of software development are applied, an overall roadmap for the project. Methods can include Agile development, waterfall, etc.";
  
- let analysisOfRequirements = ("Gather requirements to understand and document what is required by the users and other stakeholders.");
+ let analysisOfRequirements = "Gather requirements to understand and document what is required by the users and other stakeholders.";
 
- let design = ("Developing a desing around a solutions to the problems presented by the requirements, often involving process models and storyboards.");
+ let design = "Developing a desing around a solutions to the problems presented by the requirements, often involving process models and storyboards.";
 
- let implementation = ("Constructing code in the appropriate program language. Involves peer and team review to eliminate problems early and produce quality software faster.");
+ let implementation = "Constructing code in the appropriate program language. Involves peer and team review to eliminate problems early and produce quality software faster.";
 
- let testingAndIntegration = ("Testing with pre-planned scenarios as part of software design and coding and conducting performance testing to simulate load testing. Establish quality assurance priorities and release criteria to address and track defects.");
+ let testingAndIntegration = "Testing with pre-planned scenarios as part of software design and coding and conducting performance testing to simulate load testing. Establish quality assurance priorities and release criteria to address and track defects.";
 
  let maintain = "Manage and measure the project to maintain quality and delivery over the application lifecycle, and to evaluate the development process.";
